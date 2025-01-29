22264c9044a7a695ca4439c13bd7c9590f87718e
Hyungseok Kim <witbring@gmail.com>
2025-01-29 09:40:16 +0900

    Fix scripts

--- build_script/build_spec2017.py
+++ build_script/build_spec2017.py
@@ -8,14 +8,17 @@ def run(spec_path):
 
     if os.path.exists('%s/install.sh'%(src)):
 
-        cmd = 'docker run --rm -v "%s:/spec2017_image" -v "$(pwd)/src:/data" suri:v0.2 sh -c "/spec2017_image/install.sh -d /data/spec_cpu2017 -f"'%(src)
+        cmd = 'docker run --rm -v "%s:/spec2017_image" -v "$(pwd)/src:/data" suri:v1.0 sh -c "/spec2017_image/install.sh -d /data/spec_cpu2017 -f"'%(src)
         print(cmd)
         os.system(cmd)
         shutil.copyfile('script/base2017.cfg', './src/spec_cpu2017/config/base2017.cfg')
         print('[+] Successfully finish setup SPEC CPU2017')
-        cmd = 'docker run -it --rm -v "$(pwd)/src:/data" -v "$(pwd)/script:/script" -v %s:/output suri:v0.2 sh -c "/script/build-spec2017.sh"'%(output)
-        print(cmd)
-        os.system(cmd)
+
+        for opt in ['-O0', '-O1', '-O2', '-O3', '-Os', '-Ofast']:
+            for lopt in ['bfd', 'gold']: 
+                cmd = 'docker run --rm -v "$(pwd)/src:/data" -v "$(pwd)/script:/script" -v %s:/output suri:v1.0 sh -c "/script/build-spec2017.sh %s %s" '%(output, opt, lopt)
+                print(cmd)
+                os.system(cmd)
     else:
         print('[-] Install script does not exist')
 
