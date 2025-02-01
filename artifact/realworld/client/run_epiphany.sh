f607464bf4fa3e5f4bed883e86a8ba711d57cc95
hyungseok kim <witbring@kaist.ac.kr>
2025-02-01 13:17:59 +0900

    Fix

--- artifact/realworld/client/run_epiphany.sh
+++ artifact/realworld/client/run_epiphany.sh
@@ -1,2 +1,9 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/epiphany-browser/
-./my_epiphany
+#!/bin/bash
+str=$(ldd epiphany | grep libephymain.so)
+if [[ "$str" == *not* ]]; then
+    echo "Could not find library path for libephymain.so"
+else
+    libpath=$(ldd epiphany | grep libephymain.so | awk '{print $3}' | awk -F'libephymain' '{print $1}')
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$libpath
+    ./my_epiphany
+fi
