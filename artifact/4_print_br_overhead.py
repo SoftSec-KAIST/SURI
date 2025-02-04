607abcd8d65a0032ca3b61828d68880f098b4e6e
Soomin Kim <soomink@kaist.ac.kr>
2025-02-04 16:21:54 +0900

    Clean up 4_print_br_overhead.py

--- artifact/4_print_br_overhead.py
+++ artifact/4_print_br_overhead.py
@@ -1,69 +1,87 @@
 import glob
 import re
 import argparse
+import os
+
+PACKAGES = ['coreutils-9.1', 'binutils-2.40', 'spec_cpu2017', 'spec_cpu2006']
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='counter')
-    parser.add_argument('dataset', type=str, default='setA', help='Select dataset (setA, setB, setC)')
+    parser.add_argument('dataset', type=str, default='setA', help='Select dataset (setA, setC)')
 
     args = parser.parse_args()
-    assert args.dataset in ['setA', 'setC'], '"%s" is invalid. Please choose one from setA or setC.'%(args.dataset)
+    assert args.dataset in ['setA', 'setC'], 'Invalid dataset: "%s"'%(args.dataset)
 
     return args
 
-def run(base_folder):
-    res = dict()
-    res2 = dict()
+################################
+
+def read_branch_data(filepath):
+    with open(filepath) as f:
+        data = f.read()
+        (br1, br2) = re.findall('Indirect Branch Sites (.*) \((.*)\)', data.split('\n')[1])[0]
+        br1 = int(br1)
+        br2 = int(br2)
+        if br1 != 0:
+            return br2 / br1
+        else:
+            return None
+
+def collect_data(dataset):
+    base_folder = os.path.join('stat', 'bbl', dataset)
+
+    data = dict()
     for filepath in glob.glob('%s/*'%(base_folder)):
-        with open(filepath) as fd:
-            data = fd.read()
-            overhead = eval(data.split('\n')[0].split()[-1])
-            (br1, br2) =re.findall('Indirect Branch Sites (.*) \((.*)\)', data.split('\n')[1])[0]
-            if int(br1) != 0:
-                overhead2 = int(br2) / int(br1)
+        br = read_branch_data(filepath)
 
-            package, compiler = filepath.split('/')[-1].split('_')[:2]
+        package, compiler = filepath.split('/')[-1].split('_')[:2]
+        if package in ['spec']:
+            pack1, pack2, compiler = filepath.split('/')[-1].split('_')[:3]
+            package = pack1 + '_' + pack2
 
-            if package in ['spec']:
-                pack1, pack2, compiler = filepath.split('/')[-1].split('_')[:3]
-                package = pack1 + '_' + pack2
+        if package not in data:
+            data[package] = dict()
+        if compiler not in data[package]:
+            data[package][compiler] = 0, 0
+        if br is not None:
+            num_bins, overhead = data[package][compiler]
+            num_bins += 1
+            overhead += br
+            data[package][compiler] = num_bins, overhead
 
-            if package not in res:
-                res[package] = dict()
-                res2[package] = dict()
-            if compiler not in res[package]:
-                res[package][compiler] = []
-                res2[package][compiler] = []
-            res[package][compiler].append(overhead)
-            if int(br1) != 0:
-                res2[package][compiler].append(overhead2)
+    return data
 
+################################
+
+def print_header():
     print('Multi Br-------------')
 
-    tot_tot_cnt = 0
-    tot_tot_sum = 0
-    for package in ['coreutils-9.1', 'binutils-2.40', 'spec_cpu2006', 'spec_cpu2017']:
-        tot_cnt = 0
-        tot_sum = 0
+def run(args):
+    base_folder = './stat/bbl/%s'%(args.dataset)
+    data = collect_data(args.dataset)
 
-        if package not in res2:
+    print_header()
+
+    total_num_bins = 0
+    total_overhead = 0
+    for package in PACKAGES:
+        if package not in data:
             continue
 
-        for compiler in sorted(res2[package].keys()):
-            cnt = len(res2[package][compiler])
-            avg = sum(res2[package][compiler]) / cnt
-            #print('%10s %10s %10d %10f'%(package, compiler, cnt, avg*100))
-            tot_cnt += cnt
-            tot_sum += sum(res2[package][compiler])
-        tot_tot_cnt += tot_cnt
-        tot_tot_sum += tot_sum
-        print('%15s %10d %10f'%(package, tot_cnt, tot_sum/tot_cnt))
+        pkg_num_bins = 0
+        pkg_overhead = 0
+        for compiler in data[package]:
+            num_bins, overhead = data[package][compiler]
+            pkg_num_bins += num_bins
+            pkg_overhead += overhead
+        print('%15s %10d %10f' % (package, pkg_num_bins, pkg_overhead/pkg_num_bins*100))
 
-    if tot_cnt > 0:
-        print('%15s %10d %10f'%('[+]All', tot_tot_cnt, tot_tot_sum/tot_tot_cnt*100))
+        total_num_bins += pkg_num_bins
+        total_overhead += pkg_overhead
+
+    if total_num_bins > 0:
+        print('%15s %10d %10f' % ('[+]All', total_num_bins, total_overhead/total_num_bins*100))
 
 if __name__ == '__main__':
     args = parse_arguments()
-
-    base_folder = './stat/bbl/%s'%(args.dataset)
-    run(base_folder)
+    run(args)
