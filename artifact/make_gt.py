142dc55501ed21a5bef2498ad812bfbe3fa62ce6
Soomin Kim <soomink@kaist.ac.kr>
2025-02-05 16:35:40 +0900

    Clean up make_gt.py

--- artifact/make_gt.py
+++ artifact/make_gt.py
@@ -1,29 +1,30 @@
 import os
-
-def run(input_dir, output_dir):
-
-    input_dir = os.path.abspath(input_dir)
-
-    os.system('mkdir -p %s' % output_dir)
-    output_dir = os.path.abspath(output_dir)
-
-    cmd = 'docker run --rm -v %s:/data3/3_supersetCFG/benchmark -v %s:/output  suri_artifact:v1.0 sh -c "python3 /project/Reassessor/artifact/run_reassessor.py /data3/3_supersetCFG/benchmark /output"'%(input_dir, output_dir)
-    print(cmd)
-    os.system(cmd)
-
-
 import argparse
-if __name__ == '__main__':
+
+def parse_arguments():
     parser = argparse.ArgumentParser(description='GT')
-    parser.add_argument('dataset', type=str, default='setA', help='Select dataset (setA, setB, setC)')
+    parser.add_argument('dataset', type=str, default='setA', help='Select dataset (setA, setC)')
     parser.add_argument('--input_dir', type=str, default='benchmark')
     parser.add_argument('--output_dir', type=str, default='gt')
 
     args = parser.parse_args()
 
-    assert args.dataset in ['setA', 'setC'], '"%s" is invalid. Please choose one from setA or setC.'%(args.dataset)
+    assert args.dataset in ['setA', 'setC'], 'Invalid dataset: "%s"'%(args.dataset)
 
-    input_dir = '%s/%s'%(args.input_dir, args.dataset)
-    output_dir = '%s/%s'%(args.output_dir, args.dataset)
-    run(input_dir, output_dir)
+    return args
 
+def run(args):
+    in_dir = os.path.join(args.input_dir, args.dataset)
+    out_dir = os.path.join(args.output_dir, args.dataset)
+    os.system('mkdir -p %s' % out_dir)
+
+    in_dir = os.path.abspath(in_dir)
+    out_dir = os.path.abspath(out_dir)
+
+    cmd = 'docker run --rm -v %s:/data3/3_supersetCFG/benchmark -v %s:/output  suri_artifact:v1.0 sh -c "python3 /project/Reassessor/artifact/run_reassessor.py /data3/3_supersetCFG/benchmark /output"' % (in_dir, out_dir)
+    print(cmd)
+    os.system(cmd)
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    run(args)
