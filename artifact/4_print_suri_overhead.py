a3301562702c337fe51f585d99e1b3164d618c01
Soomin Kim <soomink@kaist.ac.kr>
2025-02-06 11:26:27 +0900

    Clean up 4_print_suri_overhead.py

--- artifact/4_print_suri_overhead.py
+++ artifact/4_print_suri_overhead.py
@@ -2,63 +2,100 @@ import glob
 import os
 import argparse
 
+ExpTask = namedtuple('ExpTask', ['dataset', 'log_dir', 'bin_name'])
+
+PACKAGES = ['spec_cpu2017', 'spec_cpu2006']
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('dataset', type=str, default='setA', help='Select dataset (setA)')
+
     args = parser.parse_args()
-    assert args.dataset in ['setA'], '"%s" is invalid.'%(args.dataset)
+
+    assert args.dataset in ['setA'], 'Invalid dataset: "%s"'%(args.dataset)
 
     return args
 
-def get_overhead(dataset, base_folder, package):
+def prepare_tasks(args, package):
+    tasks = []
+    for comp in COMPILERS:
+        for opt in OPTIMIZATIONS:
+            for lopt in LINKERS:
+                data_dir = os.path.join(args.dataset, package, comp, '%s_%s' % (opt, lopt))
+                log_dir = os.path.join('stat', 'suri_runtime', task.dataset, package, comp, '%s_%s' % (opt, lopt))
+                if os.path.exists(data_dir):
+                    orig_dir = os.path.join(data_dir, 'original')
+                    for target in glob.glob(orig_dir):
+                        filename = os.path.basename(target)
+                        tasks.append(ExpTask(args.dataset, log_dir, filename))
 
-    s_dict = dict()
-    s_dict['original'] = dict()
-    s_dict['suri'] = dict()
+    return tasks
 
-    for folder in glob.glob('%s/%s/%s/*/*/*'%(base_folder, dataset, package)):
-        for logfile in glob.glob('%s/*'%(folder)):
-            if 'log.txt' in logfile:
-                continue
-            with open(logfile) as fd:
-                line = fd.read().split('\n')[-2]
+################################
 
-                if 'seconds' not in line:
-                    print(line)
-                    continue
+def read_time_data(filepath):
+    with open(filepath) as f:
+        line = f.read().split('\n')[-2]
 
-                bin_name = os.path.basename(logfile)[:-4]
-                tool = folder.split('/')[-1]
-                key = '/'.join(folder.split('/')[:-1]) + bin_name
+        if 'seconds' not in line:
+            print(line)
+            return None
 
-                if tool not in ['original', 'suri']:
-                    continue
+        time = line.split(';')[1].split()[0]
+        assert line.split(';')[1].split()[1] in ['total', 'seconds']
 
+        return int(time)
 
-                val = line.split(';')[1].split()[0]
-                assert line.split(';')[1].split()[1] in ['total', 'seconds']
-                s_dict[tool][key] = int(val)
+def get_data_original(task, package):
+    log_path = os.path.join(task.log_dir, 'original', task.bin_name, '%s.txt' % task.bin_name)
+    return read_time_data(log_path)
 
-    tot = 0
-    for key in s_dict['original']:
-        tot += s_dict['suri'][key] / s_dict['original'][key]
-    return tot, len(s_dict['original'])
+def get_data_suri(task, package):
+    log_path = os.path.join(task.log_dir, 'suri', task.bin_name, '%s.txt' % task.bin_name)
+    return read_time_data(log_path)
+
+def collect_data(args, package):
+    tasks = prepare_tasks(args, tasks)
+
+    num_bins = 0
+    overhead = 0.0
+    for task in tasks:
+        d_original = get_data_original(task, package)
+        d_suri = get_data_suri(task, package)
+
+        # TODO: check None
+        num_bins += 1
+        overhead += d_suri / d_original
+
+    return num_bins, overhead
+
+################################
+
+def print_header():
+    print('%20s |  %8s  '%('', 'suri'))
+
+def print_data(package, data):
+    num_bins, overhead = data
+    if num_bins == 0:
+        return
+
+    print('%-15s %4d | %8f%%' % (package, num_bins,
+                                    overhead / len(s_dict['original'])*100-100))
 
 if __name__ == '__main__':
     args = parse_arguments()
 
-    base_folder = './stat/suri_runtime'
-    print('%20s |  %8s  '%('', 'suri'))
-    ov1 = 0
-    nu1 = 0
-    for package in ['spec_cpu2006', 'spec_cpu2017']:
-        tot1, bins1 = get_overhead(args.dataset, base_folder, package)
+    print_header()
 
-        if bins1:
-            print('%-15s %4d | %8f%%'%(package, bins1, (tot1/bins1)*100-100))
-            ov1 += tot1
-            nu1 += bins1
-    if nu1:
-        print('%-15s %4d | %8f%%'%('Total', nu1, (ov1/nu1)*100-100))
+    total_num_bins = 0
+    total_overhead = 0.0
+    for package in PACKAGES:
+        data = collect_data(args, package)
+        print_data(package, data)
 
+        num_bins, overhead = data
+        total_num_bins += num_bins
+        total_overhead += overhead
 
+    if total_num_bins != 0:
+        print('%-15s %4d | %8f%%'%('Total', total_num_bins, (total_overhead/total_num_bins)*100-100))
